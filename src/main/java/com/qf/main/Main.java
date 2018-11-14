776850549184c4509678f0b40330839189fcf541
wlb_global <wlb_global@qq.com>
2018-11-14 16:40:00 +0800

    测试第一次上传git，老大上传的项目

---
+++ src/main/java/com/qf/main/Main.java
@@ -0,0 +1,14 @@
+package com.qf.main;
+
+/**
+ * @Author wlb
+ * @Time 2018/11/14 16:28
+ * @Version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("这是用于测试git管理");
+        System.out.println("老大上传了项目");
+    }
+}
